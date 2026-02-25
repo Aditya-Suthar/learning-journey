59c220d8065ce29126491f79b3f252ebbf97f319
Aditya Suthar <adityabishnoi576@gmail.com>
2026-02-25 10:04:41 +0530

    MULTIPLE QUEUES AND MULTIPLE STACKS

---
+++ LEARNING/C DSA/P43.C
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+void enqueue(int data);
+int dequeue();
+void display();
+
+int main()
+{
+    enqueue(10);  /* INSERTING ELEMENTS INSIDE THE CIRCULAR */
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+
+    display();
+
+    printf("\nDeleted: %d", dequeue());
+    printf("\nDeleted: %d", dequeue());
+
+    enqueue(60);
+    enqueue(70);
+
+    display();
+
+    return 0;
+}
+
+void enqueue(int data)
+{
+    // Overflow condition
+    if ((rear + 1) % MAX == front)
+    {
+        printf("\nQueue Overflow");
+        return;
+    }
+
+    // First element insertion
+    if (front == -1)
+    {
+        front = rear = 0;
+    }
+    else
+    {
+        rear = (rear + 1) % MAX;
+    }
+
+    queue[rear] = data;
+}
+
+int dequeue()
+{
+    if (front == -1)
+    {
+        printf("\nQueue Underflow");
+        return -1;
+    }
+
+    int value = queue[front];
+
+    // If only one element
+    if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = (front + 1) % MAX;
+    }
+
+    return value;
+}
+
+void display()
+{
+    if (front == -1)
+    {
+        printf("\nQueue is Empty");
+        return;
+    }
+
+    printf("\nQueue elements: ");
+
+    int i = front;
+
+    while (1)
+    {
+        printf("%d ", queue[i]);
+
+        if (i == rear)
+            break;
+
+        i = (i + 1) % MAX;
+    }
+}
