dce9db5154f6d4084579012d79374b32cc9ac9d9
Aditya Suthar <adityabishnoi576@gmail.com>
2026-07-12 13:35:14 +0530

    Hashing

---
+++ LEARNING/Python/PYTHON PRACTICE/practice2.py
@@ -0,0 +1,8 @@
+#hash
+
+student = {
+    "name" : "Aditya",
+    "age"  : 25
+}
+
+print(hash("name"))
---
+++ LEARNING/Python/PYTHON PRACTICE/practice3.py
@@ -0,0 +1,8 @@
+class Person:
+    pass
+
+p1 = Person()
+p2 = Person()
+
+print(hash(p1))
+print(hash(p2))
