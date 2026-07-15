f7d7292afb95591ddf27284646592f2d25174231
Aditya Suthar <adityabishnoi576@gmail.com>
2026-07-15 23:50:47 +0530

    shallow copy

--- LEARNING/Python/shallowcopy.py
+++ LEARNING/Python/shallowcopy.py
@@ -1,10 +1,15 @@
-import copy 
+import copy
 
-lst1 = [[1,3],[2,4],4,5]
-lst2 = copy.deepcopy(lst1)
+lst1 = [[1,2],[3,4],5,6]
 
-lst2[1][0] = 5
-lst2.append(4)
+lst2 = copy.copy(lst1)
+
+lst2[1][0] = 9
+lst2.append(10)
 
 print(lst1)
 print(lst2)
+
+# lst1 = [[1,2],[9,4],5,6]
+# lst2 = [[1,2],[9,4],5,6,10]
+# expected result
